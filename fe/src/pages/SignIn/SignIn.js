33af5d34800d1889d4c20821757f79bf4b75b720
DineshReddyNEU <90339040+DineshReddyNEU@users.noreply.github.com>
2022-11-13 16:57:13 -0500

    signin
    signin

--- fe/src/pages/SignIn/SignIn.js
+++ fe/src/pages/SignIn/SignIn.js
@@ -171,7 +171,8 @@ const SignIn = () => {
     // verify user/pwd, get encoded userid as access and refresh tokens in return
     //fetch("http://localhost:5000/fastlogin", config)
     //fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/fastlogin`, config)
-    fetch(`fastlogin`, config)
+   fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
+    //fetch(`fastlogin`, config)
       .then(response => response.json())
       .then(data => {
 
